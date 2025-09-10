091ebf8380733c6aa12dc2f03570676d0d4d735e
umizu-commits <mission.mizumizu@gmail.com>
2025-09-10 18:27:13 +0900

    Update case_basic.rb

---
+++ chapter3/case_basic.rb
@@ -0,0 +1,12 @@
+language = 'Rubyist'
+
+case language
+when 'Ruby'
+  puts '私はRubyistです'
+when 'Python'
+  puts '私はPythonistaです'
+when 'PHP'
+  puts '私はPHPerです'
+else
+  puts '私はプログラマーです'
+end
---
+++ chapter3/each_loop.rb
@@ -0,0 +1,3 @@
+(1..5).each do |i|
+  puts "#{i}回目"
+end
